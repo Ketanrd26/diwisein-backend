91fd79bbf337b11d0920fae0570e7fccab749ff5
Ketan Dudka <ketandudka566123@gmail.com>
2025-01-31 16:01:27 +0530

    post origin set

--- index.js
+++ index.js
@@ -6,46 +6,58 @@ import { userRoute } from "./route/userRoute.js";
 import { contactRoute } from "./route/contactRoute.js";
 import { blogRoute } from "./route/blogroute.js";
 import path from "path";
-import http from "http"
-
+import http from "http";
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 
+const corsOption = {
+  origin: function (origin, callback) {
+    const allowedOrigins = ["https://admin.diwise.in", "https://diwise.in"];
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      // Allow the request if the origin is in the allowed list or no origin (like when testing in Postman)
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+  allowedHeaders: ["Content-Type", "Authorization"], //
+};
+app.use(cors(corsOption));
 
 app.use("/user", userRoute);
 app.use("/contact", contactRoute);
-app.use("/blog", blogRoute)
+app.use("/blog", blogRoute);
 
-app.get("/", (req,res)=>{
-    res.send("hello i am develoepr")
+app.get("/", (req, res) => {
+  res.send("hello i am develoepr");
 });
 
 const imageDirectory = "C:\\git\\gitproject\\images";
 app.use("/", express.static(imageDirectory));
 
-
 app.get("/image/:filename", (req, res) => {
-    const { filename } = req.params;
-    const filePath = path.join(imageDirectory, filename);
-    res.sendFile(filePath, (err) => {
-      if (err) {
-        res.status(404).send("Image not found");
-      }
-    });
+  const { filename } = req.params;
+  const filePath = path.join(imageDirectory, filename);
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      res.status(404).send("Image not found");
+    }
   });
-  
+});
 
 try {
-    app.listen(5000,()=>{
-        console.log("backned is running")
-    });
+  const server = http.createServer(app);
+  server.listen(5000, () => {
+    console.log("backned is running");
+  });
 
-    await checkConnection();
+  await checkConnection();
 
-    await createAlltable()
+  await createAlltable();
 } catch (error) {
-    console.log(error)
+  console.log(error);
 }
